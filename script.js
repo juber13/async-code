d530d66a70609991d9b897e90cb2a0be98b7c927
juber <juberk1010@gmail.com>
2023-10-12 10:38:41 +0530

    what is hoisting

--- script.js
+++ script.js
@@ -1,54 +1,60 @@
-// let data = [];
-//     fetch('https://jsonplaceholder.typicode.com/posts')
-//     .then(res => res.json())
-//     .then(result => console.log(result));
+// // let data = [];
+// //     fetch('https://jsonplaceholder.typicode.com/posts')
+// //     .then(res => res.json())
+// //     .then(result => console.log(result));
 
 
-    // console.log('it\'s running first')
+//     // console.log('it\'s running first')
 
-    // console.log(data)
-    const data = [];
-    const searchInput = document.querySelector('#searchInput');
-    const container = document.querySelector('.container');
+//     // console.log(data)
+//     const data = [];
+//     const searchInput = document.querySelector('#searchInput');
+//     const container = document.querySelector('.container');
     
   
 
-   // fetch data from server
-    async function fetchData(){
-        try{
-            container.innerHTML = "<p>Loding...<p>"
-            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-            const result = await res.json();
-            container.innerHTML = "";
-            return result;
-        }catch(error){
-            console.log(error);
-        }
+//    // fetch data from server
+//     async function fetchData(){
+//         try{
+//             container.innerHTML = "<p>Loding...<p>"
+//             const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//             const result = await res.json();
+//             container.innerHTML = "";
+//             return result;
+//         }catch(error){
+//             console.log(error);
+//         }
             
-    }
-    const users = async () => {
-        const re = await fetchData();
-        renderHtml(re);
-        searchText(re)
-    } 
+//     }
+//     const users = async () => {
+//         const re = await fetchData();
+//         renderHtml(re);
+//         searchText(re)
+//     } 
 
-    function renderHtml(data){
-        const displayData = Array.isArray(data) ? data.map(user => `<p class="para" style="color:red">${user.title}</p>`).join("") : "<p>No Valid Data</p>"
-        container.innerHTML =  displayData;
-        // searchText(data);
-    }
+//     function renderHtml(data){
+//         const displayData = Array.isArray(data) ? data.map(user => `<p class="para" style="color:red">${user.title}</p>`).join("") : "<p>No Valid Data</p>"
+//         container.innerHTML =  displayData;
+//         // searchText(data);
+//     }
 
-    function filterItmes(e){
-        searchText(e.target.value)
-   }
-
-
-   function searchText(data , text){
-    console.log(data ,text)
-      return data.filter(user => user.title.toLowerCase().includes(text))
-   }
+//     function filterItmes(e){
+//         searchText(e.target.value)
+//    }
 
 
-    searchInput.addEventListener('keydown' , filterItmes);
+//    function searchText(data , text){
+//     console.log(data ,text)
+//       return data.filter(user => user.title.toLowerCase().includes(text))
+//    }
 
-  users();
+
+//     searchInput.addEventListener('keydown' , filterItmes);
+
+// //   users();
+
+
+var str = "juber-khan";
+console.log(str.substring(0,4));  // jube
+console.log(str.substring(1,5));  // uber
+console.log(str.substring(1));  // uber-khan
